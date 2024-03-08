af10d93cecd1c0a086e72205a1d3c2af1eef1306
kiranreddy681 <113874933+kiranreddy681@users.noreply.github.com>
2024-03-08 09:23:09 +0530

    Add files via upload

---
+++ 29 CFG.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+int isBelongToCFG(const char* str, int index, int length) {
+    if (index == length) {
+        return 1;
+    }
+    if (isBelongToCFG(str, index, length) && str[index] == '1' &&
+        str[index + 1] == '0' && str[index + 2] == '1' &&
+        isBelongToCFG(str, index + 3, length)) {
+        return 1;
+    }
+    if (str[index] == '0' && isBelongToCFG(str, index + 1, length)) {
+        return 1;
+    }
+    if (str[index] == '1' && isBelongToCFG(str, index + 1, length)) {
+        return 1;
+    }
+    if (str[index] == '\0') {
+        return 1; 
+    }
+    return 0; 
+}
+int belongsToLanguage(const char* str) {
+    int length = strlen(str);
+    return isBelongToCFG(str, 0, length);
+}
+int main() {
+    char input[100];
+
+    printf("Enter the input string: ");
+    scanf("%s", input);
+    if (belongsToLanguage(input)) {
+        printf("Accepted: Input string belongs to the language defined by CFG\n");
+    } else {
+        printf("Rejected: Input string does not belong to the language defined by CFG\n");
+    }
+    return 0;
+}
